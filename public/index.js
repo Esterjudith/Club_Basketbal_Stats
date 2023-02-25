c1f75b493f66f8cb0b8f7ab51e9af0186cedacc8
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-02-24 22:58:19 -0700

    added menu bar & sidebar

--- public/index.js
+++ public/index.js
@@ -1,14 +1,13 @@
-// Get the hamburger menu icon and the mobile links
-const hamburger = document.querySelector(".hamburger-menu")
+const hamburger = document.querySelector(".open")
+const sidebar = document.querySelector(".sidebar")
+const closeBtn = document.querySelector(".close-btn")
 
-const mobileLinks = document.querySelector(".mobile-links")
-
-//Add a click listener to the hamburger icon
 hamburger.addEventListener("click", () => {
-    //toggle the 'active' class on the hamburger icon
-    console.log("clicked")
-    hamburger.classList.toggle('active')
+    hamburger.classList.toggle('open')
+    sidebar.classList.toggle('hidden')
+})
 
-    //toggle the 'hidden' class on the mobile links. 
-    mobileLinks.classList.toggle('hidden')
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.add("hidden")
+    hamburger.classList.remove("close")
 })
