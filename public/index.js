51c859aedca1f4ead41cf731d58216cc09b165d1
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-03-04 17:39:43 -0700

    updated a few things in the sections

--- public/index.js
+++ public/index.js
@@ -1,13 +1,9 @@
-const hamburger = document.querySelector(".open")
-const sidebar = document.querySelector(".sidebar")
-const closeBtn = document.querySelector(".close-btn")
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle('open')
-    sidebar.classList.toggle('hidden')
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
 })
 
-closeBtn.addEventListener("click", () => {
-    sidebar.classList.add("hidden")
-    hamburger.classList.remove("close")
-})
